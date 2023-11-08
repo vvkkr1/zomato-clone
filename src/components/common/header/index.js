48a178fe3a3de84ed03fc8bf9bd524a03e817db8
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-08 13:09:34 +0530

    header-right-styling

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './styles.css'
 
 const Header = () => {
   return (
-    <div className='max-width'>
-        This is Header
+    <div className='max-width header'>
+
+      <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+           alt='Zomato-logo'
+           className="header-logo"/>
+
+
+
+        <div className='header-right'>
+            <div className='header-location-search-container'>
+              <div className='location-wrapper'>
+                <div className='location-icon-name'>
+                   <i className="fi fi-rr-marker absolute-center location-icon"></i>
+                   <div>Bangalore</div>
+                </div>
+                <i class="fi fi-rr-caret-down absolute-center"></i>
+              </div>
+            </div>
+        </div>
+
+
+
     </div>
   )
 }
