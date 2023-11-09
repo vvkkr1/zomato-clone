7199791cc8e19480b83add2ba4090eeaff5b1941
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-09 17:49:56 +0530

    filter-done

--- src/components/common/filters/index.js
+++ src/components/common/filters/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './filters.css';
+import FilterItem from './filterItem';
 
-const Filters = () => {
+const Filters = ({filterList}) => {
+
   return (
-    <div>
-        This is filters
+    <div className = "filters">
+        {/* This is filters!!! */}
+        {
+          filterList && filterList.map((item)=>{
+            // console.log(item)
+            return <FilterItem filter={item} key={item.id}/>
+          })
+        }
     </div>
   )
 }
--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -2,11 +2,42 @@ import React from 'react'
 import './delivery.css'
 import Filters from '../common/filters'
 
+const deliveryFilters=[
+    {
+      id:1,
+      icon:<i className="fi fi-rr-settings-sliders"></i>,
+      title:"Filters",
+    },
+    {
+      id:2,
+      title:"Rating: 4.0+",
+    },
+    {
+      id:3,
+      title:"Safe and Hygienic",
+    },
+    {
+      id:4,
+      title:"Pure Veg"
+    },
+    {
+      id:5,
+      title:"Delivery Time",
+      icon:<i className='fi fi-rr-apps-sort absolute-center'></i>,
+    },
+    {
+      id:6,
+      title:"Great Offers"
+    }
+]
+
+
 const Delivery = () => {
   return (
     <div>
-        <Filters/>
-        Tis is delivery
+       <div className='max-width'>
+          <Filters filterList={deliveryFilters}/>
+       </div>
     </div>
   )
 }
