91f8818360c0f4aa5135853a2ac7f2cf9ebb1ed9
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-13 16:47:40 +0530

    log-out-header

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -11,17 +11,35 @@ const Header = () => {
         <div className='header-right'>
 
             <div className='header-location-search-container'>
-
+              
               <div className='location-wrapper'>
-
                 <div className='location-icon-name'>
                     <i class="fi fi-rr-marker absolute-center location-icon"></i>
                     <div>Bangalore</div>
                 </div>
                 <i class="fi fi-rr-caret-down absolute-center"></i>
-                
+              </div>
+              
+              {/* search  */}
+              <div className='location-search-separator'></div>
+              <div className='header-searchBar'>
+                 <i className="fi fi-rr-search absolute-center search-icon"></i>
+                 <input placeholder='Search for restaurant. cuisineor a dish'
+                        className='search-input'/> 
               </div>
 
+            </div>
+            
+            {/* Log out profile  */}
+            <div className='profile-wrapper'>
+                 
+                 <li>
+                     <a className='login-btn' href="index.html">Log in</a>
+                 </li>
+                 <li >
+                     <a className='signup-btn' href="index.html">Sign up</a>
+                 </li>
+                 
             </div>
 
         </div>
