aab0d38410739ecca16d80091e21d27457666272
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-13 20:05:16 +0530

    changes

--- src/components/common/tabOptions/breadCrumb/index.js
+++ src/components/common/tabOptions/breadCrumb/index.js
@@ -3,10 +3,10 @@ import './breadCrumb.css'
 
 const BreadCrumb = () => {
   return (
-    <div className='max-width ' style={{display:"flex"}}>
-        <div style={{fontSize:"16px"}}><a href="index.html" style={{textDecoration:"none", color: "rgb(130, 130, 130)",fontSize:"12px"}}><span >Home &nbsp;/</span></a></div>
-        <div style={{fontSize:"16px"}}><a href="index.html" style={{textDecoration:"none", color: "rgb(130, 130, 130)",fontSize:"12px"}}><span >&nbsp; India /</span></a></div>
-        <div style={{fontSize:"16px"}}><a href="index.html" style={{textDecoration:"none", color: "rgb(130, 130, 130)",fontSize:"12px",pointerEvents:"none"}}><span >&nbsp; Bengaluru Restaurants</span></a></div>
+    <div className='max-width breadcrumb-wrapper'>
+        <div className = 'breadcrumb-anchor-container-one' style={{fontSize:"16px"}}><a href="index.html" style={{textDecoration:"none", color: "rgb(130, 130, 130)",fontSize:"12px"}}><span >Home &nbsp;/</span></a></div>
+        <div className = 'breadcrumb-anchor-container-one' style={{fontSize:"16px"}}><a href="index.html" style={{textDecoration:"none", color: "rgb(130, 130, 130)",fontSize:"12px"}}><span >&nbsp; India /</span></a></div>
+        <div className = 'breadcrumb-anchor-container-one' style={{fontSize:"16px"}}><a href="index.html" style={{textDecoration:"none", color: "rgb(130, 130, 130)",fontSize:"12px",pointerEvents:"none", color:"rgb(207, 207, 207)"}}><span >&nbsp; Bengaluru Restaurants</span></a></div>
     </div>
   )
 }
