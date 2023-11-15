3d925c28cb55542d0eb6a204f8e457bf322d7811
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-15 22:57:59 +0530

    footer

--- src/Pages/home/index.js
+++ src/Pages/home/index.js
@@ -4,15 +4,17 @@ import TabOptions from '../../components/common/tabOptions'
 import Delivery from '../../components/delivery'
 import DiningOut from '../../components/diningOut/diningOut'
 import NightLife from '../../components/nightLife/nightLife'
+import Footer from '../../components/common/footer'
 
 const HomePage = () => {
   const [activeTab, setActiveTab]=useState('Delivery')
   return (
     <div>
-       <Header/>
-       <TabOptions activeTab={activeTab} setActiveTab={setActiveTab}/>
+       {/* <Header/> */}
+       {/* <TabOptions activeTab={activeTab} setActiveTab={setActiveTab}/> */}
        {/* Diff screens */}
-       {getSelectedScreen(activeTab)}
+       {/* {getSelectedScreen(activeTab)} */}
+       <Footer/>
     </div>
   )
 }
--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import './footerStyle.css';
 
 const Footer = () => {
   return (
-    <div>
-        This is footer
+    <div className='footer'>
+      <div className='max-width footer-wrap'>
+          <div className='footer-container'>
+              <div className='footer-image-container'>
+                  <img src='https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png?fit=around|198:42&crop=198:42;*,*'
+                       className='footer-img'
+                       alt='zomato-logo'
+                      />
+              </div>
+              
+              <div className='footer-right'>
+
+                <div className='footer-country'>
+                    <div className='india-icon-container'>
+                       <div className='india-icon'></div>
+                       <div className='india-title'>India</div>
+                       <div>
+                         <i class="fi fi-tr-caret-down"></i>
+                       </div> 
+                    </div>
+
+                  
+
+                  <div className='english-icon-container'>
+                    <div><i class="fi fi-sr-globe english-icon"></i></div>
+                    <div className='english-title'>English</div>
+                  </div>
+                </div>
+
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
